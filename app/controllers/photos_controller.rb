9f4bc2397f4ca76497e0482d80e1b23def6559c2
AmandaKJohns <amandajohns77@gmail.com>
2015-12-22 01:28:58 -0500

    Add links to nav bar
    -add footer
    -add routes for photos to display various instagram images for different usernames
    -add styling to photo and nav css

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,25 @@ class PhotosController < ApplicationController
     data = Adapters::InstagramClient.new
     founders_photos = data.find_photos('soulfounders')
     soulcycle_photos = data.find_photos('soulcycle')
-    @photos = soulcycle_photos.concat(founders_photos)
+    instructors_photos = data.find_photos('eleyna.gomez')
+    @photos = soulcycle_photos.concat(founders_photos).concat(instructors_photos)
+  end
+
+  def soulcycle
+    data = Adapters::InstagramClient.new
+    @photos = data.find_photos('soulcycle')
+  end
+
+  def founders
+    data = Adapters::InstagramClient.new
+    @photos = data.find_photos('soulfounders')
+  end
+
+  def instructors
+    data = Adapters::InstagramClient.new
+    instructor_1 = data.find_photos('eleyna.gomez')
+    instructor_2 = data.find_photos('paleoviking')
+    @photos = instructor_1.concat(instructor_2)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 
 root to: 'photos#index'
 
+get '/soulcycle' => 'photos#soulcycle'
+get '/founders' => 'photos#founders'
+get '/instructors' => 'photos#instructors'
+
 end
